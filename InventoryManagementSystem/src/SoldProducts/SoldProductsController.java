f2b64c8e98108e2f82c2055aff56b3aa37aa43d9
omkarkamate <omkar.kamate@gorillaexpense.com>
2019-03-07 19:50:28 +0530

    mail module implemented

--- InventoryManagementSystem/src/SoldProducts/SoldProductsController.java
+++ InventoryManagementSystem/src/SoldProducts/SoldProductsController.java
@@ -9,8 +9,10 @@ import Beans.SoldProductItem;
 import DatabaseHelper.DBDAO;
 import ProductEntry.ProductEntryController;
 import SellProduct.SellProductController;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -31,14 +33,29 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import jxl.Range;
+import jxl.Workbook;
+import jxl.format.Colour;
+import jxl.write.WritableCell;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
 
 /**
  * FXML Controller class
@@ -101,43 +118,76 @@ public class SoldProductsController implements Initializable {
 
     @FXML
     private void sendReportEmailAction(ActionEvent event) {
-       final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-  // Get a Properties object
-     Properties props = System.getProperties();
-     props.setProperty("mail.smtp.host", "smtp.gmail.com");
-     props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-     props.setProperty("mail.smtp.socketFactory.fallback", "false");
-     props.setProperty("mail.smtp.port", "465");
-     props.setProperty("mail.smtp.socketFactory.port", "465");
-     props.put("mail.smtp.auth", "true");
-     props.put("mail.debug", "true");
-     props.put("mail.store.protocol", "pop3");
-     props.put("mail.transport.protocol", "smtp");
-     final String username = "omskamate@gmail.com";//
-     final String password = "omkar#14u";
-     try{
-     Session session = Session.getDefaultInstance(props, 
-                          new Authenticator(){
-                             protected PasswordAuthentication getPasswordAuthentication() {
-                                return new PasswordAuthentication(username, password);
-                             }});
+    String to = "omkar.kamate@gorillaexpense.com";
 
-   // -- Create a new message --
-     Message msg = new MimeMessage(session);
+      // Sender's email ID needs to be mentioned
+      String from = "omskamate@gmail.com";
 
-  // -- Set the FROM and TO fields --
-     msg.setFrom(new InternetAddress("omskamate@gmail.com"));
-     msg.setRecipients(Message.RecipientType.TO, 
-                      InternetAddress.parse("nikteshriya@gmail.com",false));
-     msg.setSubject("Hello");
-     msg.setText("How are you");
-     msg.setSentDate(new Date());
-     Transport.send(msg);
-     System.out.println("Message sent.");
-     }catch (MessagingException e){ 
-         System.out.println("Erreur d'envoi, cause: " + e);
-     }
-     }
+      final String username = "omskamate@gmail.com";//change accordingly
+      final String password = "omkar#14u";//change accordingly
+
+      // Assuming you are sending email through relay.jangosmtp.net
+      
+
+      Properties props = new Properties();
+    props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+		props.put("mail.smtp.port", 587); //TLS Port
+		props.put("mail.smtp.auth", true); //enable authentication
+		props.put("mail.smtp.starttls.enable", true); //enable 
+
+      // Get the Session object.
+      Session session = Session.getInstance(props,
+         new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+               return new PasswordAuthentication(username, password);
+            }
+         });
+
+      try {
+         // Create a default MimeMessage object.
+         Message message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.setRecipients(Message.RecipientType.TO,
+            InternetAddress.parse(to));
+
+         // Set Subject: header field
+         message.setSubject("Testing Subject");
+
+         // Create the message part
+         BodyPart messageBodyPart = new MimeBodyPart();
+
+         // Now set the actual message
+         messageBodyPart.setText("This is message body");
+
+         // Create a multipar message
+         Multipart multipart = new MimeMultipart();
+
+         // Set text message part
+         multipart.addBodyPart(messageBodyPart);
+
+         // Part two is attachment
+         messageBodyPart = new MimeBodyPart();
+         String filename = "abc.txt";
+         DataSource source = new FileDataSource(filename);
+         messageBodyPart.setDataHandler(new DataHandler(source));
+         messageBodyPart.setFileName(filename);
+         multipart.addBodyPart(messageBodyPart);
+
+         // Send the complete message parts
+         message.setContent(multipart);
+
+         // Send message
+         Transport.send(message);
+
+         System.out.println("Sent message successfully....");
+     }  catch (Exception ex) {
+            Logger.getLogger(SoldProductsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     @FXML
     private void backAction(ActionEvent event) {
@@ -153,6 +203,25 @@ public class SoldProductsController implements Initializable {
        } catch (IOException ex) {
            Logger.getLogger(SellProductController.class.getName()).log(Level.SEVERE, null, ex);
        }
+    }
+
+    @FXML
+    private void generateAction(ActionEvent event) {
+        try {
+            LocalDate today=LocalDate.now();
+            File file=new File("dailyreport.xlsx");
+            WritableWorkbook myexcel=Workbook.createWorkbook(file);
+            WritableSheet mysheet=myexcel.createSheet("Report for "+today.toString(),0);
+          jxl.write.Label l=new  jxl.write.Label(0,0,"data1");
+           mysheet.addCell(l);
+           myexcel.write();
+           myexcel.close();
+           
+        } catch (Exception ex) {
+            Logger.getLogger(SoldProductsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }
     }
 
